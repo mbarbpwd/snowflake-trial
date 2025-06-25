8129ae6a0414083307ad75c26260773ea8705313
mbarbpwd <mbarbich@powerdataam.com>
2025-06-25 19:23:03 +0000

    testing search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,9 @@ if order_name:
 
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
 
 st.write(
   """Choose the fruits you want in your custom Smoothie!"""
