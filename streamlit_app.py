fbdcddb0d6533e4be61a6f8fa27f784e25d03833
mbarbpwd <mbarbich@powerdataam.com>
2025-06-25 19:28:35 +0000

    asd

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,11 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop
 
+#Convert the snowpark dataframe to pandas
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop
+
 st.write(
   """Choose the fruits you want in your custom Smoothie!"""
 )
